db9162a49fb07262ce75b757a469e6697ed80b5c
Angel Arciniega <choy_1_10@hotmail.es>
2017-03-16 18:51:54 -0700

    +se agrego libreria swingx +configuro tabla por titulos de arriba +mas botones +icono en el programa

---
+++ src/automata/Archivos.java
@@ -0,0 +1,112 @@
+package automata;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Archivos {
+/*
+    File entrada;
+    FileOutputStream salida;
+    BufferedReader reader;
+    BufferedWriter bw;
+    ArrayList<Ejemplos> linesFile;
+    ArrayList<Tokens> tokenPatron;
+
+    public ArrayList<Tokens> OpenToken(String direccion) {
+        try {
+            tokenPatron = new ArrayList<>();
+            entrada = new File(direccion);
+            reader = new BufferedReader(new FileReader(entrada));
+            String line;
+            int y = 0;
+            while ((line = reader.readLine()) != null) {
+                tokenPatron.add(new Tokens(line.substring(1, (line.indexOf(";") - 1)), line.substring(line.indexOf(";") + 1).trim()));
+                if (tokenPatron.get(y).getExpression().contains("//")) {
+                    String replace = tokenPatron.get(y).getExpression().replace("//", "[/][/]");
+                    tokenPatron.get(y).setExpression(replace);
+                }
+                if (tokenPatron.get(y).getExpression().contains("@")) {
+                    String replace = tokenPatron.get(y).getExpression().replace("@", "[@]");
+                    tokenPatron.get(y).setExpression(replace);
+                }
+                if (tokenPatron.get(y).getExpression().contains(".")) {
+                    String replace = tokenPatron.get(y).getExpression().replace(".", "[.]");
+                    tokenPatron.get(y).setExpression(replace);
+                }
+                if (tokenPatron.get(y).getExpression().contains(":")) {
+                    String replace = tokenPatron.get(y).getExpression().replace(":", "[:]");
+                    tokenPatron.get(y).setExpression(replace);
+                }
+                if (tokenPatron.get(y).getExpression().contains("λ")) {
+                    String replace = tokenPatron.get(y).getExpression().replace("λ", "");
+                    tokenPatron.get(y).setExpression(replace);
+                }
+                
+                y++;
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.err.println("Problema en: "+this.OpenFile(direccion) + e);
+            return null;
+        }
+        return tokenPatron;
+    }
+
+    public ArrayList<Ejemplos> OpenFile(String direccion) {
+        try {
+            linesFile = new ArrayList<>();
+            entrada = new File(direccion);
+            reader = new BufferedReader(new FileReader(entrada));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!linesFile.isEmpty()) {
+                    boolean flag = false;
+                    for (int x = 0; x < linesFile.size(); x++) {
+                        if (linesFile.get(x).getDefinicion().equals(line)) {
+                            flag = true;
+                        }
+                    }
+                    if (flag == false) {
+                        linesFile.add(new Ejemplos(line, null));
+                    }
+                } else {
+                    linesFile.add(new Ejemplos(line, null));
+                }
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.err.println("Problema en: " + e);
+            return null;
+        }
+        return linesFile;
+    }
+
+    boolean canread(String direccion) {
+        File f = new File(direccion);
+        return f.canRead();
+    }
+
+    String SaveText(File archivo, ArrayList<Ejemplos> contentFile) {
+        String respuesta;
+        try {
+            salida = new FileOutputStream(archivo);
+            bw = new BufferedWriter(new OutputStreamWriter(salida));
+            for (Ejemplos end : contentFile) {
+                bw.write(end.getDefinicion() + "\t" + end.getResultado());
+                bw.newLine();
+            }
+            bw.close();
+            respuesta = "se guardo con exito el archivo.";
+        } catch (IOException e) {
+            System.err.println("Problema en: " + e);
+            respuesta = "No se pudo guardar el archivo.";
+        }
+        return respuesta;
+    }*/
+}
+/*
+
+[a-z&&[^bc]]	a through z, except for b and c: [ad-z] (subtraction)
+[a-z&&[^m-p]]	a through z, and not m through p: [a-lq-z](subtraction)
+change value contains()"|X|" replace "|"
+*/
--- src/automata/View.java
+++ src/automata/View.java
@@ -5,6 +5,7 @@
  */
 package automata;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
@@ -12,12 +13,15 @@ import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Locale;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.text.Document;
+import org.jdesktop.swingx.prompt.PromptSupport;
 
 /**
  *
@@ -25,15 +29,29 @@ import javax.swing.text.Document;
  */
 public class View extends javax.swing.JFrame {
 
+    JFileChooser seleccionado;
+    Archivos files;
     int alf = 0;
     int est = 0;
+    ImageIcon img;
 
     public View() {
+        img = new ImageIcon("src/resources/atom-icon.png");
         initComponents();
         alfabeto.getDocument().addDocumentListener(new documentListener());
         alfabeto.getDocument().putProperty("name", "alfabeto");
         estados.getDocument().addDocumentListener(new documentListener());
         estados.getDocument().putProperty("name", "estados");
+        PromptSupport.setPrompt("Please write characteres for alphabet", alfabeto);
+        PromptSupport.setPrompt("Please write number(s) for states", estados);
+        PromptSupport.setPrompt("Please write a correct path", url);
+        PromptSupport.setFocusBehavior(PromptSupport.FocusBehavior.HIDE_PROMPT, alfabeto);
+        PromptSupport.setFocusBehavior(PromptSupport.FocusBehavior.HIDE_PROMPT, estados);
+        PromptSupport.setFocusBehavior(PromptSupport.FocusBehavior.HIDE_PROMPT, url);
+        PromptSupport.setFontStyle(Font.ITALIC | Font.BOLD, alfabeto);
+        PromptSupport.setFontStyle(Font.ITALIC | Font.BOLD, estados);
+        PromptSupport.setFontStyle(Font.ITALIC | Font.BOLD, url);
+        
     }
 
     @SuppressWarnings("unchecked")
@@ -46,12 +64,26 @@ public class View extends javax.swing.JFrame {
         alfabeto = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         tabla = new javax.swing.JTable();
+        saveBtn = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jComboBox1 = new javax.swing.JComboBox();
+        minimizeBtn = new javax.swing.JButton();
+        searchBtn = new javax.swing.JButton();
+        url = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setIconImage(img.getImage());
 
         jLabel1.setText("Alfabeto: ");
 
         jLabel2.setText("Estados: ");
+
+        alfabeto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                alfabetoActionPerformed(evt);
+            }
+        });
 
         tabla.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -63,30 +95,78 @@ public class View extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tabla);
 
+        saveBtn.setBackground(new java.awt.Color(255, 255, 255));
+        saveBtn.setFont(new java.awt.Font("Helvetica", 0, 14)); // NOI18N
+        saveBtn.setText("Save");
+        saveBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                saveBtnActionPerformed(evt);
+            }
+        });
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane2.setViewportView(jTable1);
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Automate form file", "Automate from table" }));
+
+        minimizeBtn.setText("Minimize");
+
+        searchBtn.setText("Search");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(21, 21, 21)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(saveBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(25, 25, 25))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(21, 21, 21)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel1))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel1))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(estados, javax.swing.GroupLayout.PREFERRED_SIZE, 344, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(alfabeto, javax.swing.GroupLayout.PREFERRED_SIZE, 344, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 499, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(63, 63, 63)
+                                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(url)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(searchBtn)))
+                                .addGap(188, 188, 188)))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(alfabeto, javax.swing.GroupLayout.PREFERRED_SIZE, 344, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(estados, javax.swing.GroupLayout.PREFERRED_SIZE, 344, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 499, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(70, Short.MAX_VALUE))
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(minimizeBtn))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(65, 65, 65)
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(minimizeBtn)
+                    .addComponent(searchBtn)
+                    .addComponent(url, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(alfabeto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -95,12 +175,41 @@ public class View extends javax.swing.JFrame {
                     .addComponent(jLabel2)
                     .addComponent(estados, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 387, Short.MAX_VALUE)
-                .addGap(18, 18, 18))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 429, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(saveBtn)
+                .addGap(6, 6, 6))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void saveBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveBtnActionPerformed
+        /*files = new Archivos();
+         seleccionado = new JFileChooser();
+         if (!contenido.getText().trim().equals("")) {
+         if (seleccionado.showDialog(null, "Save File") == JFileChooser.APPROVE_OPTION) {
+         archivo = seleccionado.getSelectedFile();
+         if (archivo.getName().endsWith("txt")) {
+         String cont = contenido.getText();
+         String respuesta = files.SaveText(archivo, contentFile);
+         JOptionPane.showMessageDialog(null, respuesta);
+         } else {
+         JOptionPane.showMessageDialog(null, "The file most be saved as .txt extenssion");
+         }
+         }
+         } else {
+         JOptionPane.showMessageDialog(null, "Sorry... You have to select correct tokens and sample files first ☺");
+         }*/
+    }//GEN-LAST:event_saveBtnActionPerformed
+
+    private void alfabetoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_alfabetoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_alfabetoActionPerformed
 
     public static void main(String args[]) {
         try {
@@ -114,7 +223,7 @@ public class View extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(View.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        
+
         //</editor-fold>
 
         /* Create and display the form */
@@ -126,10 +235,17 @@ public class View extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField alfabeto;
     private javax.swing.JTextField estados;
+    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JButton minimizeBtn;
+    private javax.swing.JButton saveBtn;
+    private javax.swing.JButton searchBtn;
     private javax.swing.JTable tabla;
+    private javax.swing.JTextField url;
     // End of variables declaration//GEN-END:variables
 class documentListener implements DocumentListener {
 
@@ -167,7 +283,18 @@ class documentListener implements DocumentListener {
                     break;
             }
             if (est > 0 && alf > 0) {
-                model = new DefaultTableModel(est, alf);
+                Object values[][]=new Object[est][alf];
+                String titulos[]=new String[alf];
+                for(int x=0;x<alf;x++){
+                    if((x+1)<alf){
+                    titulos[x]=alfabeto.getText().substring(x, x+1);    
+                    }else{
+                        titulos[x]=alfabeto.getText().substring(x);
+                    }
+                    System.out.println(titulos[x]);
+                }
+                System.out.println();
+                model = new DefaultTableModel(values, titulos);
                 tabla.setModel(model);
             } else {
                 tabla.setModel(new DefaultTableModel(0, 0));
