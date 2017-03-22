f6cb9ac474af9ce39b42217c1f36aab1b53cfc38
Gabriela <Gabriela@DESKTOP-IVPE56S>
2017-03-21 20:07:03 -0700

    ya casi chatos

--- src/automata/Minimaze.java
+++ src/automata/Minimaze.java
@@ -1,15 +1,14 @@
 package automata;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class Minimaze {
 
-    Integer estados[];
+    int estados[];
     ArrayList<Integer> aceptacion;
     ArrayList<String> alfabeto;
-    ArrayList<Integer[]> transicion;
-    ArrayList<Integer[]> resultado;
+    ArrayList<int[]> transicion;
+    ArrayList<int[]> resultado;
 
     public static void main(String[] args) {
         Minimaze min = new Minimaze();
@@ -26,44 +25,34 @@ public class Minimaze {
         alfabeto.add("1");
         //MATRIZ INICIAL se usara para comparar con las demas matrices
         transicion = new ArrayList();
-        estados = new Integer[2];
+        estados = new int[2];
         estados[0] = 1;
         estados[1] = 2;
         transicion.add(estados);
-        estados = new Integer[2];
+        estados = new int[2];
         estados[0] = 4;
         estados[1] = 3;
         transicion.add(estados);
-        estados = new Integer[2];
+        estados = new int[2];
         estados[0] = 5;
         estados[1] = 3;
         transicion.add(estados);
-        estados = new Integer[2];
+        estados = new int[2];
         estados[0] = 3;
         estados[1] = 3;
         transicion.add(estados);
-        estados = new Integer[2];
+        estados = new int[2];
         estados[0] = 4;
         estados[1] = 4;
         transicion.add(estados);
-        estados = new Integer[2];
+        estados = new int[2];
         estados[0] = 5;
         estados[1] = 5;
         transicion.add(estados);
-        System.out.println(transicion.size());
 
-        int cant = 0;
-        boolean flag = false;
-        for (int y = 0; y < aceptacion.size(); y++) {
-//si algunos de los estados de aceptacion vienen siendo el primero entonces 
-            if (0 == aceptacion.get(y)) {
-                flag = true;
-            }
-        }
-
-        ArrayList<Integer[]> matrices = new ArrayList();
-        Integer acep[] = new Integer[aceptacion.size()];
-        Integer noAcep[] = new Integer[transicion.size() - aceptacion.size()];
+        ArrayList<int[]> matrices = new ArrayList();
+        int acep[] = new int[aceptacion.size()];
+        int noAcep[] = new int[transicion.size() - aceptacion.size()];
         int si = 0;
         int no = 0;
         for (int x = 0; x < acep.length; x++) {
@@ -71,7 +60,7 @@ public class Minimaze {
         }
         for (int x = 0; x < transicion.size(); x++) {
             boolean flag2 = false;
-            for (Integer acep1 : acep) {
+            for (int acep1 : acep) {
                 if (x == acep1) {
                     flag2 = true;
                 }
@@ -82,18 +71,18 @@ public class Minimaze {
             }
         }
 // recursividad(matrices);
-        ArrayList<Integer[]> sol = new ArrayList();
-        Integer acep1[] = new Integer[1];
+        ArrayList<int[]> sol = new ArrayList();
+        int acep1[] = new int[1];
         acep1[0] = 0;
         sol.add(acep1);
-        acep1 = new Integer[1];
+        acep1 = new int[1];
         acep1[0] = 3;
         sol.add(acep1);
-        acep1 = new Integer[2];
+        acep1 = new int[2];
         acep1[0] = 1;
         acep1[1] = 2;
         sol.add(acep1);
-        acep1 = new Integer[2];
+        acep1 = new int[2];
         acep1[0] = 4;
         acep1[1] = 5;
         sol.add(acep1);
@@ -101,38 +90,39 @@ public class Minimaze {
 
     }
 
-    public void recursividad(ArrayList<Integer[]> matrices) {
+    public void recursividad(ArrayList<int[]> matrices) {
 
     }
 /// cuando recibimos los estados ya minimizados ({0,1}{5}{2,3,4})ahora creamos nuevos estados solidos ({0}{1}{2}) 
 
-    public void solucion(ArrayList<Integer[]> matrices) {
+    public void solucion(ArrayList<int[]> matrices) {
         ArrayList<Integer> nuevaAceptacion = new ArrayList();
         for (int x = 0; x < matrices.size(); x++) {
-            Integer temp2[] = transicion.get(matrices.get(x)[0]);
-            for (Integer temp21 : temp2) {
-                for (int z = 0; z < aceptacion.size(); z++) {
-                    if (Objects.equals(temp21, aceptacion.get(z))) {
-                        for (int j = 0; j < nuevaAceptacion.size(); j++) {
-                            if (x != nuevaAceptacion.get(j)) {
-                                nuevaAceptacion.add(x);
+            for (int y = 0; y < aceptacion.size(); y++) {
+                if (matrices.get(x)[0] == aceptacion.get(y)) {
+                    nuevaAceptacion.add(x);
+                }
+            }
+            System.out.println(x);
+        }
+        aceptacion = nuevaAceptacion;
+        resultado = new ArrayList();
+        int resultadoVector[];
+        for (int y = 0; y < matrices.size(); y++) {
+            resultadoVector = new int[alfabeto.size()];
+            for (int x = 0; x < resultadoVector.length; x++) {
+                int nueva[] = transicion.get(matrices.get(y)[0]);
+                for (int nueva1 : nueva) {
+                    for (int w = 0; w < matrices.size(); w++) {
+                        for (int get : matrices.get(w)) {
+                            if (get == nueva1) {
+                                resultadoVector[x] = w;
                             }
                         }
                     }
                 }
             }
-            aceptacion = nuevaAceptacion;
-            for (Integer temp21 : temp2) {
-                for (int y = 0; y < matrices.size(); y++) {
-                    Integer resultadoVector[] = new Integer[matrices.get(0).length];
-                    for (int z = 0; z < matrices.get(y).length; z++) {
-                        if (Objects.equals(temp21, matrices.get(y)[z])) {
-                            resultadoVector[z] = y;
-                        }
-                    }
-                    resultado.add(resultadoVector);
-                }
-            }
+            resultado.add(resultadoVector);
         }
         transicion = resultado;
     }
